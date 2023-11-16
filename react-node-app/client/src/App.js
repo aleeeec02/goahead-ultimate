790a36c5c9f2f0b8adb5cf2e15f6bdfb48aea59b
TomasNM229 <67438238+TomasNM229@users.noreply.github.com>
2023-11-15 19:01:57 -0500

    Topbar y Login

--- react-node-app/client/src/App.js
+++ react-node-app/client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Topbar from './Topbar';
+import FormularioLogin from './formulario-login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar />
+      <div className="content">
+        <FormularioLogin/>
+        {/* Agrega aquí el resto del contenido principal de tu aplicación */}
+      </div>
     </div>
   );
 }
--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hola desde el servidor!" });
