b819b1d5a4293b8974d48f2d820231124e76b957
aleeeec02 <conzahualpa@gmail.com>
2023-11-17 12:08:10 -0500

    Itinerarios (pantalla): agregar dinamismo y respectivo CSS al componente

--- react-node-app/client/src/Pages/Itinerarios.js
+++ react-node-app/client/src/Pages/Itinerarios.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import Topbar from './Topbar';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../style-pages/itinerario.css';
-import Rutas from '../rutas';
 
 function Itinerarios() {
-  return (
+  const [itinerarios, setItinerarios] = useState([]);
 
-        <div>
-          <h1>Itinerario</h1>
-          <p>Usted no cuenta con itinerarios registrados.</p>
-          <button>Crear nuevo itinerario</button>
-          {}
-        </div>
+  // Suponiendo que tienes un endpoint en tu servidor para obtener los itinerarios
+  useEffect(() => {
+    const fetchItinerarios = async () => {
+      try {
+        const response = await fetch('/api/itinerarios');
+        if (!response.ok) {
+          throw new Error('La respuesta de la red no estuvo bien');
+        }
+        const data = await response.json();
+        setItinerarios(data);
+      } catch (error) {
+        console.error('Error al obtener itinerarios:', error);
+      }
+    };
+
+    fetchItinerarios();
+  }, []);
+
+  return (
+    <div className="itinerarios-container">
+      <h1>Itinerario</h1>
+      {itinerarios.length ? (
+        <ul>
+          {itinerarios.map((itinerario, index) => (
+            <li key={index}>
+              {itinerario.nombre} - {itinerario.destino}
+              {[/*[TODO***: Agregar más detalles aquí] */]}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Usted no cuenta con itinerarios registrados.</p>
+      )}
+      <Link to="/crear-itinerario" className="btn btn-primary">Crear nuevo itinerario</Link>
+    </div>
   );
 }
 
