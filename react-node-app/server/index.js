3229ce1cca507f10586d324c87f04b71cdd18335
aleeeec02 <conzahualpa@gmail.com>
2023-11-22 05:58:51 +0000

    Cambios de rediccionamiento de archivos servidos en index.js

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -58,10 +58,10 @@ app.post('/api/itinerarios', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "../../client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
